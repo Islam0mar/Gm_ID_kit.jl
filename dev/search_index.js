e982ef27cd35ea005fec469c46f8a5b0f857df5b
Documenter.jl <documenter@juliadocs.github.io>
2022-12-03 00:08:00 +0000

    build based on 4d99d90

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Gm_ID_kit","category":"page"},{"location":"#Gm*ID*kit","page":"Home","title":"GmIDkit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GmIDkit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Gm_ID_kit]","category":"page"}]
+}
